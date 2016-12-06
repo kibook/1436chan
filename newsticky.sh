f77e68aa2b4bda4d107ffc25b0eba67e73e00840
kibook <annadaniau@gmail.com>
2016-12-06 14:11:41 -0500

    ensure new threads have group write

--- newsticky.sh
+++ newsticky.sh
@@ -3,6 +3,7 @@
 timestamp=`date +%s`
 
 mkdir sticky_$timestamp
+chmod g+w sticky_$timestamp
 ln template_gophermap sticky_$timestamp/gophermap
 ln template_post sticky_$timestamp/post
 echo "$@" > sticky_$timestamp/gophertag
