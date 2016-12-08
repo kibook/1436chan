dc29e8f50b2ccb5fbde2813224d491c3bfe7359d
kibook <annadaniau@gmail.com>
2016-12-08 14:25:47 -0500

    backup motd and params.sh

--- backup.sh
+++ backup.sh
@@ -7,4 +7,4 @@ then
 	mkdir backups
 fi
 
-tar zcvf backups/backup_$(date +%s).tar.gz [0-9]* sticky_* $CHAN_ARCHIVE/[0-9]* 2>/dev/null
+tar zcvf backups/backup_$(date +%s).tar.gz [0-9]* sticky_* $CHAN_ARCHIVE/[0-9]* motd params.sh 2>/dev/null
