1858f50aa2798764cf7c29cee59c00574433d901
kibook <annadaniau@gmail.com>
2016-12-08 14:19:42 -0500

    update backup and wipe scripts

--- backup.sh
+++ backup.sh
@@ -1,3 +1,10 @@
-cd "${0%/*}"
+#!/bin/sh
 
-tar zcvf backups/backup_`date +%s`.tar.gz * --exclude='*.tar.gz'
+. ./params.sh
+
+if [ ! -e "backups" ]
+then
+	mkdir backups
+fi
+
+tar zcvf backups/backup_$(date +%s).tar.gz [0-9]* sticky_* $CHAN_ARCHIVE/[0-9]* 2>/dev/null
--- wipe.sh
+++ wipe.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+
+. ./params.sh
 
 echo "WARNING: THIS WILL DELETE EVERYTHING!"
 read -p "Are you sure you want to continue? y/n [n]: " CONT
@@ -7,7 +9,7 @@ if [ "$CONT" = "y" ]
 then
 	rm -rf [0-9]*
 	rm -rf sticky_*
-	rm -rf archive/[0-9]*
+	rm -rf "$CHAN_ARCHIVE"/[0-9]*
 else
 	echo "Aborted."
 fi
