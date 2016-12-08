0e27887ed542afcb6c679550804470fe13a5ca76
kibook <annadaniau@gmail.com>
2016-12-08 11:07:25 -0500

    add missing $

--- archive.sh
+++ archive.sh
@@ -10,7 +10,7 @@ ln template_archive_gophermap $1/gophermap
 
 for post in $(ls $1/[0-9]* 2>/dev/null)
 do
-	comment=$(cat post | sed "s/\\$CHAN_ROOT\/$1/\\$CHAN_ROOT\/$CHAN_ARCHIVE\/$1/g")
+	comment=$(cat $post | sed "s/\\$CHAN_ROOT\/$1/\\$CHAN_ROOT\/$CHAN_ARCHIVE\/$1/g")
 	echo "$comment" > $post
 done
 
