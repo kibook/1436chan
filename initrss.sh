5a4028cd96ef6d886a0d10fcfe06dc810df6a4aa
kibook <annadaniau@gmail.com>
2018-02-13 15:19:43 -0500

    Include type in gopher URI

--- initrss.sh
+++ initrss.sh
@@ -6,6 +6,6 @@ cp -n template_rss.xml rss.xml
 
 xmlstarlet ed -L \
 	-u /rss/channel/title -v "$SERVER_HOST$CHAN_ROOT" \
-	-u /rss/channel/link -v "gopher://$SERVER_HOST:$SERVER_PORT$CHAN_ROOT" \
+	-u /rss/channel/link -v "gopher://$SERVER_HOST:$SERVER_PORT/1$CHAN_ROOT" \
 	-u /rss/channel/description -v "$SERVER_HOST$CHAN_ROOT" \
 	rss.xml
