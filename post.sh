4a3ca2992398365ea851ae18d4ae16d8cf955c98
kibukj <kibuzhj@gmail.com>
2019-12-30 11:08:32 -0500

    Update post.sh to handle tripcode/post ID

--- post.sh
+++ post.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if ! test -e ./params.sh
+then
+	exit 1
+fi
+
+. ./params.sh
+
 thread=$1
 
 if [ ! "$thread" ]
@@ -20,13 +27,28 @@ no=$(($no + 1))
 
 post="$stamp"_"$no"
 
+# post id
+if test "$ENABLE_POST_IDS" = y
+then
+	postid=$(sh tripcode.sh "$SALT_DIR" "$SERVER_HOST")
+	echo "${no} ${postid}" >> postids
+fi
+
+# tripcode
+read -p "Tripcode: " password
+if test -n "$password"
+then
+	tripcode=$(sh tripcode.sh "$SALT_DIR" "$password")
+	echo "${no} ${tripcode}" >> tripcodes
+fi
+
 touch $thread/$post
 vi $thread/$post
 sh updatepostcache.sh $thread $post >> $thread/postcache
 
 if test "$MAX_RSS_ITEMS" -gt 0
 then
-	sh updaterss.sh $id $no "$(cat $thread/$post)"
+	sh updaterss.sh $thread $no "$(cat $thread/$post)"
 fi
 
 echo $no > posts
